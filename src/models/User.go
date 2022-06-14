28fd6feaa5fdc0e3ac6fbd850c174121ea5b800e
Luccas Barros <ldbcandido@gmail.com>
2022-06-14 11:57:07 -0300

    feat: create format user data package

--- src/models/User.go
+++ src/models/User.go
@@ -1,8 +1,12 @@
 package models
 
-import "time"
+import (
+	"errors"
+	"strings"
+	"time"
+)
 
-// omitempty - se o campo ID tiver em branco, não passa o ID pro JSON e remove o campo ID.
+// omitempty - If field is empty then the property will be omitted
 type User struct {
 	ID        uint64    `json:"id,omitempty"`
 	Name      string    `json:"name,omitempty"`
@@ -10,4 +14,41 @@ type User struct {
 	Email     string    `json:"email,omitempty"`
 	Password  string    `json:"password,omitempty"`
 	CreatedAt time.Time `json:"created_at,omitempty"`
+}
+
+// Prepare will call validate and formatt methods to validate and format the received user data
+func (user *User) Prepare() error {
+	if erro := user.validate(); erro != nil {
+		return erro
+	}
+
+	user.format()
+
+	return nil
+}
+
+func (user *User) validate() error {
+	if user.Name == "" {
+		return errors.New("Name is required and cannot be empty")
+	}
+
+	if user.Nick == "" {
+		return errors.New("Nick is required and cannot be empty")
+	}
+
+	if user.Email == "" {
+		return errors.New("Email is required and cannot be empty")
+	}
+
+	if user.Password == "" {
+		return errors.New("Password is required and cannot be empty")
+	}
+
+	return nil
+}
+
+func (user *User) format() {
+	user.Name = strings.TrimSpace(user.Name)
+	user.Nick = strings.TrimSpace(user.Nick)
+	user.Email = strings.TrimSpace(user.Email)
 }
