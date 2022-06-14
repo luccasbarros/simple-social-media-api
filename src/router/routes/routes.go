684122cd79ce27380bd6fc964b85fcab09474824
Luccas Barros <ldbcandido@gmail.com>
2022-06-14 09:30:32 -0300

    feat: add simple router config

--- src/router/routes/routes.go
+++ src/router/routes/routes.go
@@ -1,6 +1,10 @@
 package routes
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
 
 // All API Routes
 type Route struct {
@@ -8,4 +12,15 @@ type Route struct {
 	Method                 string
 	Function               func(http.ResponseWriter, *http.Request)
 	AuthenticationRequired bool
+}
+
+// Configure put all routes into router configuration
+func Configure(r *mux.Router) *mux.Router {
+	routes := usersRoutes
+
+	for _, route := range routes {
+		r.HandleFunc(route.URI, route.Function).Methods(route.Method)
+	}
+
+	return r
 }
