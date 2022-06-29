de5c256b5800be08f2283e998352becc35e8f5ce
Luccas Barros <ldbcandido@gmail.com>
2022-06-29 11:27:29 -0300

    db: add sql query to create followers table

--- sql/sql.sql
+++ sql/sql.sql
@@ -2,6 +2,7 @@ CREATE DATABASE IF NOT EXISTS devbook;
 USE devbook;
 
 DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS followers;
 
 CREATE TABLE users (
   id int auto_increment primary key,
@@ -11,3 +12,15 @@ CREATE TABLE users (
   password varchar(100) NOT NULL,
   created_at timestamp DEFAULT CURRENT_TIMESTAMP()
 ) ENGINE=INNODB;
+
+CREATE TABLE followers (
+  user_id int not null, 
+  FOREIGN KEY (user_id) REFERENCES users (id)
+  ON DELETE CASCADE,
+
+  follower_id int not null,
+  FOREIGN KEY (follower_id) REFERENCES users (id)
+  ON DELETE CASCADE,
+
+  primary key (user_id, follower_id)
+  ) ENGINE=InnoDB;
